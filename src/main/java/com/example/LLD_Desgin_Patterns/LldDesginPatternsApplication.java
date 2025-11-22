b8600dc5f5603fcc77fd3adae69de68abdcf7646
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-11-22 16:30:45 +0530

    LLD-Desgin_patterns

---
+++ src/main/java/com/example/LLD_Desgin_Patterns/LldDesginPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.example.LLD_Desgin_Patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldDesginPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LldDesginPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LLD_Desgin_Patterns/LldDesginPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LLD_Desgin_Patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldDesginPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
